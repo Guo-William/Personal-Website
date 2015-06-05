a5c4c85458846cf4387f0434a94187d8bb0ebb9c
William Guo <william.wenzhou.guo@gmail.com>
2015-06-05 14:02:52 -0400

    Nav bar Works
    Nav bar now works
    it has some kinks will fix soon

---
+++ includes.js
@@ -0,0 +1,18 @@
+var navigation  = new Array();  // This is for the navigation.
+
+// ==================== Navigation ==================== //
+navigation[0]   = '<div id="tab_navigation">';
+navigation[1]   = '<ul class="toolbar" id="dropline">';
+navigation[2]   = '<li class="link-home"><a class="home" href="/"> HOME </a></li>';
+navigation[3]   = '<li class="menu-divide"><a class="reg-link" href="pages/Projects.html"> Projects </a></li>';
+navigation[4]   = '<li class="menu-item"><a class="reg-link" href="pages/Resume.html"> Resume </a></li>';
+navigation[5]   = '</ul>';
+navigation[6]   = '</div>';
+
+function show(i)
+ {
+  for (x in i)
+  {
+   document.write(i[x]+'\n')
+  }
+ }
