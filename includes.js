a590dd60ed8e395258f93b06fdfafb26e97931f4
William Guo <william.wenzhou.guo@gmail.com>
2015-06-05 14:04:22 -0400

    Fix
    Link from one sub page to another crashes this is a quick fix.

--- includes.js
+++ includes.js
@@ -4,8 +4,8 @@ var navigation  = new Array();  // This is for the navigation.
 navigation[0]   = '<div id="tab_navigation">';
 navigation[1]   = '<ul class="toolbar" id="dropline">';
 navigation[2]   = '<li class="link-home"><a class="home" href="/"> HOME </a></li>';
-navigation[3]   = '<li class="menu-divide"><a class="reg-link" href="pages/Projects.html"> Projects </a></li>';
-navigation[4]   = '<li class="menu-item"><a class="reg-link" href="pages/Resume.html"> Resume </a></li>';
+navigation[3]   = '<li class="menu-divide"><a class="reg-link" href="/pages/Projects.html"> Projects </a></li>';
+navigation[4]   = '<li class="menu-item"><a class="reg-link" href="/pages/Resume.html"> Resume </a></li>';
 navigation[5]   = '</ul>';
 navigation[6]   = '</div>';
 
