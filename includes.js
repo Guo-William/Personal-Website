7476c2b37942f0bd83043114dd0f6c6d2de56770
isignisign <275030039bh@gmail.com>
2015-07-04 22:03:22 -0400

    Made some suggested changes

--- includes.js
+++ includes.js
@@ -3,11 +3,12 @@ var navigation  = new Array();  // This is for the navigation.
 // ==================== Navigation ==================== //
 navigation[0]   = '<div id="tab_navigation">';
 navigation[1]   = '<ul class="toolbar" id="dropline">';
-navigation[2]   = '<li class="link-home"><a class="home" href="/"> HOME </a></li>';
-navigation[3]   = '<li class="menu-divide"><a class="reg-link" href="/pages/Projects.html"> Projects </a></li>';
-navigation[4]   = '<li class="menu-item"><a class="reg-link" href="/pages/Resume.html"> Resume </a></li>';
-navigation[5]   = '</ul>';
-navigation[6]   = '</div>';
+navigation[2]   = '<li class="top-menu"> MENU </li>';
+navigation[3]   = '<li class="menu-divide"><a class="reg-link" href="/"> Home </a></li>';
+navigation[4]   = '<li class="menu-item"><a class="reg-link" href="/pages/Projects.html"> Projects </a></li>';
+navigation[5]   = '<li class="menu-item"><a class="reg-link" href="/pages/Resume.html"> Resume </a></li>';
+navigation[6]   = '</ul>';
+navigation[7]   = '</div>';
 
 function show(i)
  {
