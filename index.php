52e5a890e001da3ec6d1a9e17a952808b699b537
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 16:48:31 -0500

    took it ouy

--- index.php
+++ index.php
@@ -21,10 +21,8 @@
 </head>
 
 <body>
+  <?php require('includes/navbar.php');?>
   <div>
-    <!-- There is require here-->
-    <!--<script type="text/javascript">show(navigation);</script>-->
-    <?php require('../includes/navbar.php');?>
     <div class="head-shot col-xs-12 col-sm-6 col-md-3 col-lg-3">
       <img class="head" src="images\headshot.jpg" alt="Headshot"
         style="width:96%; height:96%;">
