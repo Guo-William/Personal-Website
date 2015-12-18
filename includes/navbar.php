d9cbb62ba612a410c1f3711f90b3cccb258e5d7f
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 01:19:22 -0500

    forgot to change paths

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,8 +2,8 @@
   <ul class="toolbar" id="dropline">
     <li class="top-menu"> MENU </li>
     <li class="menu-divide"><a class="reg-link" href="/"> Home </a></li>
-    <li class="menu-item"><a class="reg-link" href="/pages/Projects.html"> Projects </a></li>
-    <li class="menu-item"><a class="reg-link" href="/pages/Resume.html"> Resume </a></li>
+    <li class="menu-item"><a class="reg-link" href="/pages/Projects.php"> Projects </a></li>
+    <li class="menu-item"><a class="reg-link" href="/pages/Resume.php"> Resume </a></li>
   </ul>
 </div>
     
