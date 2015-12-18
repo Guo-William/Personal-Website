872f8affead30261d8bf735391293eba1e538fb3
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 17:06:45 -0500

    Bad centering fix

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
     </div>
     <div class="head-shot col-xs-12 col-sm-3 col-md-2 col-lg-2">
       <div class="col-xs-2"></div>
-      <div class="head-shot col-xs-10">
+      <div class="head-shot col-xs-10 col-sm-12 col-md-12 col-lg-12">
         <img class="head" src="images\headshot.jpg" alt="Headshot"
           style="width:96%; height:96%;">
       </div>
