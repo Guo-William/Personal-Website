48f494ffe8955f2e0a28d0b1900f90756345fcfb
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 18:13:25 -0500

    took out row

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-  <div class="row">
+  <div>
     <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
       <?php require('includes/navbar.php');?>
     </div>
