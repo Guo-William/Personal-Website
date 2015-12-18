850e8e3923d17d9e560ab679ac2f5821eacb4f04
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 16:46:03 -0500

    Testing navbar further

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
     <!-- There is require here-->
     <!--<script type="text/javascript">show(navigation);</script>-->
     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-      <?php require('../includes/navbar.php');?>
+      <!--<?php require('../includes/navbar.php');?>-->
     </div>
-      <div class="head-shot col-xs-12 col-sm-6 col-md-3 col-lg-3">
-        <img class="head" src="images\headshot.jpg" alt="Headshot"
+    <div class="head-shot col-xs-12 col-sm-6 col-md-3 col-lg-3">
+      <img class="head" src="images\headshot.jpg" alt="Headshot"
         style="width:96%; height:96%;">
-      </div>
+    </div>
     <div class="theTop col-xs-12 col-sm-6 col-md-3 col-lg-3">
       <p class="name">
         <br>
