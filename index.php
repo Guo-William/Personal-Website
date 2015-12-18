c7c786293a65c3a3ec667c03520142a2874d847e
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 16:43:41 -0500

    Making it one entire div

--- index.php
+++ index.php
@@ -21,10 +21,12 @@
 </head>
 
 <body>
-  <?php require('includes/navbar.php');?>
   <div>
     <!-- There is require here-->
     <!--<script type="text/javascript">show(navigation);</script>-->
+    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+      <?php require('/includes/navbar.php');?>
+    </div>
       <div class="head-shot col-xs-12 col-sm-6 col-md-3 col-lg-3">
         <img class="head" src="images\headshot.jpg" alt="Headshot"
         style="width:96%; height:96%;">
