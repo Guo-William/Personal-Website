bf2c2340c567af83eabba64aff17b04f6fdbaa61
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-18 17:42:02 -0500

    testing out this one

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,22 +1,57 @@
-<div class="sidebar-nav">
-  <nav class="navbar navbar-inverse" role="navigation">
+<!--<div class="sidebar-nav">-->
+<!--  <nav class="navbar navbar-inverse" role="navigation">-->
   <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="/">Home</a>
+<!--  <div class="navbar-header">-->
+<!--    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">-->
+<!--      <span class="sr-only">Toggle navigation</span>-->
+<!--      <span class="icon-bar"></span>-->
+<!--      <span class="icon-bar"></span>-->
+<!--      <span class="icon-bar"></span>-->
+<!--    </button>-->
+<!--    <a class="navbar-brand" href="/">Home</a>-->
+<!--  </div>-->
+  
+  
+<!--  <div class="collapse navbar-collapse navbar-ex1-collapse">-->
+<!--    <ul class="nav navbar-nav">-->
+<!--      <li><a href="/pages/Projects.php">Projects</a></li>-->
+<!--      <li><a href="/pages/Resume.php">Resume</a></li>-->
+<!--    </ul>-->
+<!--  </div><!-- /.navbar-collapse -->-->
+<!--</nav>-->
+<!--</div>-->
+
+<div class="sidebar-nav">
+      <div class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <span class="visible-xs navbar-brand">Sidebar menu</span>
+        </div>
+        <div class="navbar-collapse collapse sidebar-navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Menu Item 1</a></li>
+            <li><a href="#">Menu Item 2</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Menu Item 4</a></li>
+            <li><a href="#">Reviews <span class="badge">1,118</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
   </div>
-  
-  
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <ul class="nav navbar-nav">
-      <li><a href="/pages/Projects.php">Projects</a></li>
-      <li><a href="/pages/Resume.php">Resume</a></li>
-    </ul>
-  </div><!-- /.navbar-collapse -->
-</nav>
-</div>
