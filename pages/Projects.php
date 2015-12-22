2d5efe5e43648ded4c609f233cc2116b35c0150d
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 21:45:47 -0500

    testing out container

--- pages/Projects.php
+++ pages/Projects.php
@@ -14,13 +14,16 @@
 </head>
 
 <body>
-  <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
   </div>
-  <div class="row">
-    <div class="col-xs-12 col-sm-5 col-md-5 col-lg-3 col-lg-offset-1">
+  
+  <div class="col-xs-12 col-sm-5 col-md-5 col-lg-3 col-lg-offset-1">
     <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   </div>
+    </div>
   </div>
   
   
