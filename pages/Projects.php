1f630526dfe870b530da40962d07f0b3fea17b9e
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-22 00:32:38 -0500

    jquery path lost

--- pages/Projects.php
+++ pages/Projects.php
@@ -7,7 +7,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Latest compiled JavaScript -->
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <script src="jquery-2.1.4.js"></script>
+  <script src="../jquery-2.1.4.js"></script>
   <!--<script type="text/javascript" src="../includes.js"></script>-->
   <link rel="stylesheet" href="../CSS/styling.css">
 </head>
