c08cf6086df3c3de117c8e5bd863ae144a81de35
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 19:48:45 -0500

    changed anemoslips sm to 6

--- pages/ProjectPieces/AnemoSlips.php
+++ pages/ProjectPieces/AnemoSlips.php
@@ -1,4 +1,4 @@
-<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
   <div class=project-blue>
     <img class="anemoslips-img" src="..\images\AnemoSlips.jpg" alt="Innoservice: AnemoSlips">
     <p class="project-description">
