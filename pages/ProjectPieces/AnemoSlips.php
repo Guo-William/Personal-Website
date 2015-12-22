a5dbcb93cf53aecf0c6a68cfe9513101bad6dfff
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 19:45:48 -0500

    Making projects into seperate Pieces so that I can reuse em how I please

---
+++ pages/ProjectPieces/AnemoSlips.php
@@ -0,0 +1,11 @@
+<div class="col-xs-12 col-sm-10 col-md-11 col-lg-11">
+  <div class=project-blue>
+    <img class="anemoslips-img" src="..\images\AnemoSlips.jpg" alt="Innoservice: AnemoSlips">
+    <p class="project-description">
+      <b>Innoservice: AnemoSlips</b> <br>
+        A product to clean phones and hands <br>
+        Researched product background and marketing <br>
+        2014-Present
+    </p>
+  </div>
+</div>
--- pages/Projects.php
+++ pages/Projects.php
@@ -17,16 +17,6 @@
   <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
   </div>
-  <div class="col-xs-12 col-sm-10 col-md-11 col-lg-11">
-    <div class=project-blue>
-      <img class="anemoslips" src="..\images\AnemoSlips.jpg" alt="Innoservice: AnemoSlips">
-      <p class="project-description">
-        <b>Innoservice: AnemoSlips</b> <br>
-        A product to clean phones and hands <br>
-        Researched product background and marketing <br>
-        2014-Present
-      </p>
-    </div>
-  </div>
+  <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
 </body>
 </html>
