4f0dbd1cd3dd5e47360654b2a0db357e7785b7e2
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 20:56:25 -0500

    going to use offset-1

--- pages/Projects.php
+++ pages/Projects.php
@@ -17,13 +17,13 @@
   <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
   </div>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
+  <div class="hidden-xs hidden-sm hidden-md col-offset-1"></div>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1" style="width:100px;"></div>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1" style="width:100px;"></div>
+  <div class="hidden-xs hidden-sm hidden-md col-offset-1"></div>
+  <div class="hidden-xs hidden-sm hidden-md col-offset-1"></div>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
