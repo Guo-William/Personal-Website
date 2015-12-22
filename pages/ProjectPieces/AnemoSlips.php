b4bcd3ae1687f3550ffbd1c91e49cae203f97412
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 23:03:10 -0500

    caption should now be centered

--- pages/ProjectPieces/AnemoSlips.php
+++ pages/ProjectPieces/AnemoSlips.php
@@ -1,6 +1,6 @@
   <div class=project-blue>
     <img class="anemoslips-img" src="..\images\AnemoSlips.jpg" alt="Innoservice: AnemoSlips">
-    <figcaption><b>Innoservice: AnemoSlips</b> <br></figcaption>
+    <figcaption class="project-cap"><b>Innoservice: AnemoSlips</b> <br></figcaption>
     <p class="project-description">
       
         A product to clean phones and hands <br>
