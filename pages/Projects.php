5389e2ce0750170321381231021d45dfd728ba3c
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 21:47:51 -0500

    going to use container-fluid

--- pages/Projects.php
+++ pages/Projects.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
