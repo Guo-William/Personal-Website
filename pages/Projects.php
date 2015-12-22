a5b5d3c15487a626291bcc69fc46289f1583a5ec
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 20:06:12 -0500

    took away middle ground

--- pages/Projects.php
+++ pages/Projects.php
@@ -21,14 +21,13 @@
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
+  
   
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
-  <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
+  
+  
   <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
   <div class="hidden-xs hidden-sm hidden-md col-lg-1"></div>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
