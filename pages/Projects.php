ab1fc15799781e664fd48096f965f5b780d4839d
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 20:58:44 -0500

    correctly using offset

--- pages/Projects.php
+++ pages/Projects.php
@@ -17,7 +17,11 @@
   <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
   </div>
-  <div class="hidden-xs hidden-sm hidden-md col-offset-1"></div>
+  <!--<div class="col-xs-12 col-sm-5 col-md-5 col-lg-3 col-off-">-->
+    
+  <!--</div>-->
+  
+  <div class="hidden-xs hidden-sm hidden-md col-lg-1 col-lg-offset-1"></div>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
   <?php require('../pages/ProjectPieces/AnemoSlips.php');?>
