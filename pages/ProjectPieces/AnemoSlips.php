aefa53a1b416315e98dffedcfc60e42db3e9cfb5
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-21 22:26:33 -0500

    figcaptioned for Anemoslips

--- pages/ProjectPieces/AnemoSlips.php
+++ pages/ProjectPieces/AnemoSlips.php
@@ -1,7 +1,8 @@
   <div class=project-blue>
     <img class="anemoslips-img" src="..\images\AnemoSlips.jpg" alt="Innoservice: AnemoSlips">
+    <figcaption><b>Innoservice: AnemoSlips</b> <br></figcaption>
     <p class="project-description">
-      <b>Innoservice: AnemoSlips</b> <br>
+      
         A product to clean phones and hands <br>
         Researched product background and marketing <br>
         2014-Present
