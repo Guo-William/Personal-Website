f4ed133c641e83757ecd4a49eb5cf56c1d1f2827
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-22 23:06:37 -0500

    cant nest containers

--- pages/Resume.php
+++ pages/Resume.php
@@ -19,8 +19,7 @@
   </div>
   <div class="container">
     
-    <div class="container">
-    
+    <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
       <b>Education</b>
     </div>
