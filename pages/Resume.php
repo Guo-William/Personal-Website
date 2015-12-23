18815c7c71d70b5af4fa3810e3616b8a6e992bbe
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-22 22:47:18 -0500

    Fixing some row problems

--- pages/Resume.php
+++ pages/Resume.php
@@ -20,7 +20,8 @@
   <div class="container-fluid">
     
     <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
         <b>Education</b>
       </div>
       <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
@@ -31,9 +32,8 @@
       <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
         2014 - Present
       </div>
-    </div>
-    
-    <div class="row">
+      </div>
+      <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
         <b>Work Experience</b>
       </div>
@@ -50,6 +50,9 @@
       </div>
       
     </div>
+    </div>
+    
+    
     
   </div>
 </body>
