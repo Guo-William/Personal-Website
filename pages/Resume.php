d93385b85636dcae9e9702c33eb2e7ecaa44dc29
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-22 23:13:04 -0500

    row inside row

--- pages/Resume.php
+++ pages/Resume.php
@@ -23,37 +23,38 @@
   
   
     <div class="col-xs-12 col-sm-10 col-md-11 col-lg-11">
-    <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-      <b>Education</b>
-    </div>
-    <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
-      Northeastern Univerity, Boston, MA<br>
-      College of Computer and Information Science<br>
-      Candidate for Bachelors of Science in Computer Science
-    </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+          <b>Education</b>
+        </div>
+        <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
+          Northeastern Univerity, Boston, MA<br>
+          College of Computer and Information Science<br>
+          Candidate for Bachelors of Science in Computer Science
+        </div>
       
-    <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
-      2014 - Present
-    </div>
-    
-        
-    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-      <b>Work Experience</b>
-    </div>
+        <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
+          2014 - Present
+        </div>
+      </div>
       
-    <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
-      Fundamentals of Computer Science I Tutor
-      <b>Northeastern University</b>
-      <ul>
-        <li>Place Hold</li>
-        <li>Place Hold</li>
-        <li>Place Hold</li>
-      </ul>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+          <b>Work Experience</b>
+        </div>
+      
+        <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
+          Fundamentals of Computer Science I Tutor
+          <b>Northeastern University</b>
+          <ul>
+            <li>Place Hold</li>
+            <li>Place Hold</li>
+            <li>Place Hold</li>
+          </ul>
+        </div>
+      </div>
     </div>
     </div>
-  </div>
-  </div>
   </div>
 </body>
 </html>
