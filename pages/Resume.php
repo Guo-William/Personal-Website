dd50c7decd19b8f7f605079ed84b7f1a19612d78
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-22 22:45:56 -0500

    Filling up resume page

--- pages/Resume.php
+++ pages/Resume.php
@@ -17,5 +17,40 @@
   <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
     <?php require('../includes/navbar.php');?>
   </div>
+  <div class="container-fluid">
+    
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+        <b>Education</b>
+      </div>
+      <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
+        Northeastern Univerity, Boston, MA<br>
+        College of Computer and Information Science<br>
+        Candidate for Bachelors of Science in Computer Science
+      </div>
+      <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
+        2014 - Present
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+        <b>Work Experience</b>
+      </div>
+      <div class="row">
+        <div class="col-xs-11 col-sm-11 col-md-8 col-lg-8">
+          Fundamentals of Computer Science I Tutor
+          <b>Northeastern University</b>
+          <ul>
+            <li>Place Hold</li>
+            <li>Place Hold</li>
+            <li>Place Hold</li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
+    
+  </div>
 </body>
 </html>
