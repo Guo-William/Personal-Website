9dbc47261c1529e84bf85239e7534fc50c153341
Guo-William <william.wenzhou.guo@gmail.com>
2015-12-26 00:45:08 -0500

    syntax error found

--- includes/navbar.php
+++ includes/navbar.php
@@ -23,7 +23,7 @@
   // var wanted_width = $(".img-pad").css("margin-left");
   // $(".img-pad").css("margin-top", wanted_margin);
   alert(42);
-  var wanted_width = $(".navbar-stretch").parent().width());
+  var wanted_width = $(".navbar-stretch").parent().width();
   alert(wanted_width);
   $(".navbar-stretch").width($(".navbar-stretch").parent().width());
   
