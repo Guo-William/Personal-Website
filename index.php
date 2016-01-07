de4433c27ff923bd18ab147f5146a5f4a63d6b4a
William Guo <william.wenzhou.guo@gmail.com>
2016-01-07 01:53:44 -0500

    Turned it into a fluid container

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
             <?php require('includes/navbar.php');?>
         </div>
