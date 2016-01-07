da1000c2acf325595c2a77cb662e0679b5d4a259
William Guo <william.wenzhou.guo@gmail.com>
2016-01-07 01:41:44 -0500

    Adding contacts/icons

--- index.php
+++ index.php
@@ -2,46 +2,55 @@
 <html>
 
 <head>
-  <title>William Guo</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="William Guo's personal website">
-  <meta name="keywords" content="William Guo, Guo, William">
-  <meta name="author" content="William Guo">
+    <title>William Guo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="William Guo's personal website">
+    <meta name="keywords" content="William Guo, Guo, William">
+    <meta name="author" content="William Guo">
 
 
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <!-- Latest compiled JavaScript -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <script src="jquery-2.1.4.js"></script>
-  <!--<script type="text/javascript" src="includes/includes.js"></script> -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="jquery-2.1.4.js"></script>
+    <!--<script type="text/javascript" src="includes/includes.js"></script> -->
 
-  <link rel="stylesheet" href="CSS/styling.css">
+    <link rel="stylesheet" href="CSS/styling.css">
+    <link rel="stylesheet" href="font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome.css">
 </head>
 
 <body>
-  <div>
-    <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
-      <?php require('includes/navbar.php');?>
-    </div>
-    <div class="head-shot col-xs-12 col-sm-3 col-md-2 col-lg-2">
-      <img class="center-block" src="images\headshot.jpg" alt="Headshot"
-      style="width:96%; height:96%;">
-    </div>
-    <div class="theTop col-xs-12 col-sm-7 col-md-9 col-lg-9">
-      <p class="name">
-        <br>
-        Undergraduate At Northeastern University, Boston, MA<br>
-        Candidate for Bachelors of Science in Computer Science<br>
-        Fundamentals of Computer Science I Tutor at Northeastern University
-      </p>
+    <div>
+        <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
+            <?php require('includes/navbar.php');?>
+        </div>
+        <div class="head-shot col-xs-12 col-sm-3 col-md-2 col-lg-2">
+            <img class="center-block" src="images\headshot.jpg" alt="Headshot"
+            style="width:96%; height:96%;">
+        </div>
+        <div class="theTop col-xs-12 col-sm-7 col-md-9 col-lg-9">
+            <p class="name">
+                <br>
+                Undergraduate At Northeastern University, Boston, MA<br>
+                Candidate for Bachelors of Science in Computer Science<br>
+                Fundamentals of Computer Science I Tutor at Northeastern University
+            </p>
+        </div>
+
     </div>
 
-  </div>
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">Contacts should go here</p>
+            <i class="fa fa-github fa-3x"></i>
+            <i class="fa fa-linkedin fa-3x"></i>
+        </div>
+    </footer>
 </body>
-
 
 </html>
