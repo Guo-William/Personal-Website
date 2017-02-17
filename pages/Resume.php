1613c18782e2fcf3e03fd0f422a753f7b71523f0
William Guo <william.wenzhou.guo@gmail.com>
2017-02-17 01:20:56 -0500

    Ipad Pro fixes

--- pages/Resume.php
+++ pages/Resume.php
@@ -17,9 +17,9 @@
   <br><br>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
-        <?php require('../includes/navbar.php');?>
-      </div>
+      <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1" style="padding:0;margin:0;">
+            <?php require('includes/navbar.php');?>
+        </div>
 
 
 
