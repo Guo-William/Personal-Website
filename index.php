947a109811ec52971c2e132b44eb93ac3d51794f
William Guo <william.wenzhou.guo@gmail.com>
2017-02-17 01:12:38 -0500

    Responsive fixes
    For IPad mainly. They needa stop making tablets  T_T

--- index.php
+++ index.php
@@ -26,16 +26,19 @@
 
 <body>
     <div>
+        <!-- spacing -->
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><br><br><br><br></div>
-        <div class="col-xs-12 col-sm-2 col-md-1 col-lg-1" style="padding:0;margin:0;">
+        <!-- nav bar -->
+        <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1" style="padding:0;margin:0;">
             <?php require('includes/navbar.php');?>
         </div>
-        <div class="col-xs-12 col-sm-3 col-md-1 col-lg-1"></div>
+
+        <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1"></div>
         <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2" style="padding:0">
             <img class="center-block" src="images\headshot.jpg" alt="Headshot"
             style="width:100%; height:100%; padding:0">
         </div>
-        <div class="theTop col-xs-12 col-sm-4 col-md-7 col-lg-7">
+        <div class="theTop col-xs-12 col-sm-8 col-md-7 col-lg-7">
             <p class="name">
                 <span style="font-size: 250%; color: #a1b56c;">William Guo</span><br>
                 Undergraduate at Northeastern University, Boston, MA<br>
@@ -43,7 +46,7 @@
             </p>
         </div>
 
-        <div class="col-xs-12 col-sm-2 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <br><br>
             <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-2 col-lg-offset-2" >
                 <b class="resume-title" style="color: #333">Updates</b>
@@ -60,7 +63,7 @@
           
         </div>
 
-        <div class="col-xs-12 col-sm-2 col-md-12 col-lg-12" style="background-color: #181818; z-index: -10">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="background-color: #181818; z-index: -10">
             <div class="col-xs-12 col-sm-12 col-md-2 col-lg- col-md-offset-2 col-lg-offset-2" >
                 <br>
                 <b class="resume-title" style="color: #a1b56c">About</b>
@@ -76,22 +79,23 @@
             </div>
         </div>
 
-
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="position: absolute; bottom:0;">
-            <p class="text-muted" style="margin-left: auto;margin-right: auto; text-align:center;">
-                <span style="font-size: 200%">Contact</span><br>
-                Send me an email at:
-                <a href="mailto:william.wenzhou.guo@gmail.com">william.wenzhou.guo@gmail.com</a>
-            </p>
-            <div class="col-lg-5"></div>
-            <div class="col-lg-2">
-                <div class="col-lg-5"></div>
-                <a href="https://github.com/Guo-William" target="_blank"><i class="fa fa-github fa-3x"></i></a>
-                <a href="https://www.linkedin.com/in/williamwenzhouguo" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a>
-                <div class="col-lg-5"></div>
+        <div class="container jankStuff">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <p class="text-muted" style="margin-left: auto;margin-right: auto; text-align:center; ">
+                    <span style="font-size: 200%">Contact</span><br>
+                    Send me an email at:
+                    <a href="mailto:william.wenzhou.guo@gmail.com">william.wenzhou.guo@gmail.com</a>
+                </p>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5"></div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-2" style="margin-left: auto;margin-right: auto; text-align:center;">
+                    <a href="https://github.com/Guo-William" target="_blank"><i class="fa fa-github fa-3x"></i></a>
+                    <a href="https://www.linkedin.com/in/williamwenzhouguo" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5"></div>
             </div>
-            <div class="col-lg-5"></div>
         </div>
+        
+        
     </div>
 
 </body>
